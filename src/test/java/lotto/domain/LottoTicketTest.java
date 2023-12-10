92c70c3b37c3a2009b7a4798840ed7a66d92e0d5
COBI-98 <tkdgus968@naver.com>
2023-12-11 04:07:54 +0900

    test: 발행한 로또 티켓의 로또 별 순위 리스트 생성 테스트

--- src/test/java/lotto/domain/LottoTicketTest.java
+++ src/test/java/lotto/domain/LottoTicketTest.java
@@ -1,5 +1,11 @@
 package lotto.domain;
 
+import static lotto.domain.Ranking.FIFTH;
+import static lotto.domain.Ranking.FIRST;
+import static lotto.domain.Ranking.FOURTH;
+import static lotto.domain.Ranking.NONE;
+import static lotto.domain.Ranking.SECOND;
+import static lotto.domain.Ranking.THIRD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
@@ -18,5 +24,41 @@ class LottoTicketTest {
 
         //when //then
         assertThat(lottoTicket.lottoTicket()).hasSize(2);
+    }
+
+    @DisplayName("calculateWinningStatistic() : 발행한 로또 티켓의 로또 별 순위 리스트 생성")
+    @Test
+    void calculateWinningStatistic_success() throws Exception{
+        //given
+        LottoTicket autoTicket = createAutoTicket();
+        LottoDrawingMachine lottoDrawingMachine = startLottoDrawingMachine();
+
+        //when
+        List<Ranking> rankings = autoTicket.calculateWinningStatistic(lottoDrawingMachine);
+
+        //then
+        assertThat(rankings).hasSize(6)
+                .containsExactlyInAnyOrder(
+                        FIRST, SECOND, THIRD, FOURTH, FIFTH, NONE
+                );
+    }
+
+    private static LottoTicket createAutoTicket() {
+        Lotto createLotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto createLotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        Lotto createLotto3 = new Lotto(List.of(1, 2, 3, 4, 5, 45));
+        Lotto createLotto4 = new Lotto(List.of(1, 2, 3, 4, 44, 45));
+        Lotto createLotto5 = new Lotto(List.of(1, 2, 3, 42, 43, 45));
+        Lotto createLotto6 = new Lotto(List.of(1, 2, 41, 42, 43, 45));
+
+
+        return new LottoTicket(List.of(createLotto1, createLotto2, createLotto3,
+                createLotto4, createLotto5, createLotto6));
+    }
+
+    private static LottoDrawingMachine startLottoDrawingMachine() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoBall bonusBall = new LottoBall("7");
+        return new LottoDrawingMachine(lotto, bonusBall);
     }
 }
