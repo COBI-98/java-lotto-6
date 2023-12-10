11e626e9de404dae78b162207e30ffa9bf77ddcc
COBI-98 <tkdgus968@naver.com>
2023-12-10 22:50:39 +0900

    test: 로또 당첨 번호를 비교하여 순위 계산 기능 추가

--- src/test/java/lotto/domain/LottoDrawingMachineTest.java
+++ src/test/java/lotto/domain/LottoDrawingMachineTest.java
@@ -3,14 +3,18 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import lotto.domain.Lotto;
 import lotto.domain.LottoBall;
 import lotto.domain.LottoDrawingMachine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoDrawingMachineTest {
 
@@ -42,5 +46,39 @@ class LottoDrawingMachineTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(exceptionMessage);
 
+    }
+    
+    @DisplayName("calculateRanking() : 플레이어가 발행한 로또와 로또 당첨 번호를 비교하여 순위 계산")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void calculateRanking_success(List<Integer> playerLotto, Ranking expectedRanking) throws Exception{
+        //given
+        LottoDrawingMachine lottoDrawingMachine = startLottoDrawingMachine();
+        Lotto lotto = new Lotto(playerLotto);
+
+        //when
+        Ranking ranking = lottoDrawingMachine.calculateRanking(lotto);
+
+        //then
+        assertThat(ranking).isEqualTo(expectedRanking);
+    }
+
+    private static LottoDrawingMachine startLottoDrawingMachine() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        LottoBall bonusBall = new LottoBall("7");
+        return new LottoDrawingMachine(lotto, bonusBall);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), Ranking.FIRST),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 7), Ranking.SECOND),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 45), Ranking.THIRD),
+                Arguments.of(Arrays.asList(1, 2, 3, 4, 44, 45), Ranking.FOURTH),
+                Arguments.of(Arrays.asList(1, 2, 3, 43, 44, 45), Ranking.FIFTH),
+                Arguments.of(Arrays.asList(1, 2, 42, 43, 44, 45), Ranking.NONE),
+                Arguments.of(Arrays.asList(1, 41, 42, 43, 44, 45), Ranking.NONE),
+                Arguments.of(Arrays.asList(40, 41, 42, 43, 44, 45), Ranking.NONE)
+        );
     }
 }
